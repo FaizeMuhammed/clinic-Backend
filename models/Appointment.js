dc3dd18f36f5df2b0dfc75b07ccd535c852f43a8
FaizeMuhammed <168710031+FaizeMuhammed@users.noreply.github.com>
2025-02-17 22:18:29 +0530

    updated

--- models/Appointment.js
+++ models/Appointment.js
@@ -5,7 +5,8 @@ const appointmentSchema = new mongoose.Schema({
   patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true }, // Reference to Patient
   date: { type: String, required: true }, // Format: YYYY-MM-DD
   time: { type: String, required: true }, // Format: '9:00 AM'
-  status: { type: String, enum: ['Scheduled', 'Completed', 'Cancelled'], default: 'Scheduled' }
+  status: { type: String, enum: ['Scheduled', 'Completed', 'Cancelled'], default: 'Scheduled' },
+  type: { type: String, enum: ['New Patient', 'Revisit', 'Follow-up'], required: true } // New field
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);
